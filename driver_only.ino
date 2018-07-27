946f1b1c0d1ac6428d2a5c0d13df6bcb408e625c
Mayank Senapati <mkxx1997@gmail.com>
2018-07-27 19:16:16 +0530

    Add files via upload

---
+++ driver_only.ino
@@ -0,0 +1,57 @@
+char val;
+
+void setup() {
+pinMode(2,OUTPUT);    //left wheel
+pinMode(3,OUTPUT);
+pinMode(4,OUTPUT);    //right wheel
+pinMode(5,OUTPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+if (Serial.available())
+{
+  val = Serial.read();
+}
+  if (val == 'F')
+  {
+    digitalWrite(2,LOW);
+    digitalWrite(3,HIGH);
+    digitalWrite(4,HIGH);
+    digitalWrite(5,LOW);
+  }
+
+  else if (val == 'B')
+  {
+    digitalWrite(2,HIGH);
+    digitalWrite(3,LOW);
+    digitalWrite(4,LOW);
+    digitalWrite(5,HIGH);
+  }
+   else if (val == 'R') //
+  {
+    digitalWrite(2,LOW);
+    digitalWrite(3,HIGH);
+    digitalWrite(4,LOW);
+    digitalWrite(5,LOW);
+  }
+
+  else if (val == 'L') //
+  {
+    digitalWrite(2,LOW);
+    digitalWrite(3,LOW);
+    digitalWrite(4,HIGH);
+    digitalWrite(5,LOW);
+  }
+
+  else if (val == 'S')
+  {
+    digitalWrite(2,LOW);
+    digitalWrite(3,LOW);
+    digitalWrite(4,LOW);
+    digitalWrite(5,LOW);
+  }
+
+}
+
+
